d1de58e5aa68a1cc25141450777d2a4b6162ced5
Roberto Giuseppe Maver <roberto67@MacBook-Air-di-Roberto.local>
2026-08-14 13:43:00 +0200

    fix(ui): remove projector mode elements and clean git tracking

--- .next/standalone/.next/server/chunks/_next-internal_server_app_api_seed_route_actions_168eb8_.js
+++
@@ -1,3 +0,0 @@
-module.exports=[60612,s=>{"use strict";s.s([])}];
-
-//# sourceMappingURL=_next-internal_server_app_api_seed_route_actions_168eb8_.js.map
--- .next/standalone/.next/server/chunks/ssr/_next-internal_server_app_documentos_page_actions_1kjfv5x.js
+++
@@ -1,3 +0,0 @@
-module.exports=[17731,a=>{"use strict";a.s([])}];
-
-//# sourceMappingURL=_next-internal_server_app_documentos_page_actions_1kjfv5x.js.map
