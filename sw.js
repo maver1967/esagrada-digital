c8687bda13ab9c72e3168e65b704417a01edd02e
Roberto Giuseppe Maver <roberto67@MacBook-Air-di-Roberto.local>
2026-08-14 11:36:53 +0200

    fix(cache): unregister stale PWA service worker and force cache buster v4.2.4-clean-spa

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    }).then(function() {
+      return self.registration.unregister();
+    })
+  );
+});
